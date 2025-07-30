6cd70b4065f020c120bca3fa9bee20bb474caf1c
devbattery <devbattery@outlook.com>
2025-07-30 19:49:44 +0900

    initial commit

---
+++ src/main/java/com/lion/be/LionChatBeApplication.java
@@ -0,0 +1,13 @@
+package com.lion.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LionChatBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LionChatBeApplication.class, args);
+    }
+
+}
