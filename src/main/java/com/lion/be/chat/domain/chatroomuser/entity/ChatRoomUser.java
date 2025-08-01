7d3a340c3aa359a8d4badf8805b1f16871a530ec
devbattery <devbattery@outlook.com>
2025-08-01 12:25:51 +0900

    Fix #5: ChatRoom 관계 정립

--- src/main/java/com/lion/be/chat/domain/chatroomuser/entity/ChatRoomUser.java
+++ src/main/java/com/lion/be/chat/domain/chatroomuser/entity/ChatRoomUser.java
@@ -1,22 +1,39 @@
 package com.lion.be.chat.domain.chatroomuser.entity;
 
+import com.lion.be.chat.domain.chatroom.entity.ChatRoom;
+import com.lion.be.user.domain.entity.User;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
-import lombok.*;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
-@Setter
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor
 public class ChatRoomUser {
+
     @Id
     private Long id;
 
-    private Long chatRoomId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private ChatRoom chatRoom;
 
-    private Long userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
 
     private Boolean isRead;
+
+    private LocalDateTime regDt;
+
+    public ChatRoomUser(Boolean isRead, LocalDateTime regDt) {
+        this.isRead = isRead;
+        this.regDt = LocalDateTime.now();
+    }
+
 }
--- src/main/java/com/lion/be/user/domain/entity/User.java
+++ src/main/java/com/lion/be/user/domain/entity/User.java
@@ -1,11 +1,16 @@
 package com.lion.be.user.domain.entity;
 
+import com.lion.be.chat.domain.chatroomuser.entity.ChatRoomUser;
 import com.lion.be.user.domain.Role;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -27,6 +32,9 @@ public class User {
     private String imageUrl;
 
     private Role role;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ChatRoomUser> chatRoomUsers = new ArrayList<>();
 
     public User(String name, String email, String imageUrl, Role role) {
         this.name = name;
