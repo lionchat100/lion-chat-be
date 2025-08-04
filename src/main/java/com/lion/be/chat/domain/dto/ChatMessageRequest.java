3a1b4ef6162422fc1668c8b27b2719debe94cc55
devbattery <devbattery@outlook.com>
2025-08-04 13:09:31 +0900

    Fix #23: Name -> Email 변경

--- src/main/java/com/lion/be/chat/domain/dto/ChatMessageRequest.java
+++ src/main/java/com/lion/be/chat/domain/dto/ChatMessageRequest.java
@@ -1,5 +1,6 @@
 package com.lion.be.chat.domain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,6 +8,7 @@ import lombok.Setter;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class ChatMessageRequest {
