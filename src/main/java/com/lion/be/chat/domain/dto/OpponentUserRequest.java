8cad401c817af1e531063751c3fd41bf1af221cb
devbattery <devbattery@outlook.com>
2025-08-04 13:09:16 +0900

    Fix #23: DTO 기본 생성자, Setter 추가

--- src/main/java/com/lion/be/chat/domain/dto/OpponentUserRequest.java
+++ src/main/java/com/lion/be/chat/domain/dto/OpponentUserRequest.java
@@ -1,12 +1,14 @@
 package com.lion.be.chat.domain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
-@Getter
+@AllArgsConstructor
 @Setter
+@Getter
 public class OpponentUserRequest {
     private Long id;
 }
