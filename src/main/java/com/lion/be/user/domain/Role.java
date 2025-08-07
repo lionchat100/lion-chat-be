7d91f8957dd56606f3dda07d834a43aecec582ed
W.llama <106076670+W-llama@users.noreply.github.com>
2025-08-08 00:05:51 +0900

    Add: BANNED 추가

--- src/main/java/com/lion/be/user/domain/Role.java
+++ src/main/java/com/lion/be/user/domain/Role.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Role {
 
+    BANNED("ROLE_BANNED","차단된유저"),
     USER("ROLE_USER", "유저"),
     ADMIN("ROLE_ADMIN", "관리자"),
     STAFF("ROLE_STAFF", "직원");
