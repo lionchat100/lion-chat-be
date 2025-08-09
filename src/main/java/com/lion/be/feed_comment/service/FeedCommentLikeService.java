f10cddb26c315639542b6bcb0ecd4ed900a7f0d8
LSH-kw0315 <tmdgjsdl1234@naver.com>
2025-08-09 22:15:04 +0900

    Feat: RedisKey 전역 설정 빼먹은 것

--- src/main/java/com/lion/be/feed_comment/service/FeedCommentLikeService.java
+++ src/main/java/com/lion/be/feed_comment/service/FeedCommentLikeService.java
@@ -1,6 +1,8 @@
 package com.lion.be.feed_comment.service;
 
 import java.util.Objects;
+
+import com.lion.be.global.util.RedisKey;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -11,27 +13,23 @@ public class FeedCommentLikeService {
 
     private final RedisTemplate<String, Object> redisTemplate;
 
-    private static final String LIKE_COUNT_KEY_PREFIX = "comment:like_count:";
-    private static final String LIKED_USERS_KEY_PREFIX = "comment:liked_users:";
-    private static final String DIRTY_COMMENTS_KEY = "dirty:comments";
-
     public void likeComment(Long commentId, Long userId) {
-        String likedUsersKey = LIKED_USERS_KEY_PREFIX + commentId;
+        String likedUsersKey = RedisKey.COMMENT_LIKED_USERS_KEY_PREFIX + commentId;
         String userIdStr = String.valueOf(userId);
 
         if (Objects.equals(redisTemplate.opsForSet().add(likedUsersKey, userIdStr), 1L)) {
-            redisTemplate.opsForValue().increment(LIKE_COUNT_KEY_PREFIX + commentId);
-            redisTemplate.opsForSet().add(DIRTY_COMMENTS_KEY, String.valueOf(commentId));
+            redisTemplate.opsForValue().increment(RedisKey.COMMENT_LIKE_COUNT_KEY_PREFIX + commentId);
+            redisTemplate.opsForSet().add(RedisKey.DIRTY_COMMENT_LIKE_KEY, String.valueOf(commentId));
         }
     }
 
     public void unlikeComment(Long commentId, Long userId) {
-        String likedUsersKey = LIKED_USERS_KEY_PREFIX + commentId;
+        String likedUsersKey = RedisKey.COMMENT_LIKED_USERS_KEY_PREFIX + commentId;
         String userIdStr = String.valueOf(userId);
 
         if (Objects.equals(redisTemplate.opsForSet().remove(likedUsersKey, userIdStr), 1L)) {
-            redisTemplate.opsForValue().decrement(LIKE_COUNT_KEY_PREFIX + commentId);
-            redisTemplate.opsForSet().add(DIRTY_COMMENTS_KEY, String.valueOf(commentId));
+            redisTemplate.opsForValue().decrement(RedisKey.COMMENT_LIKE_COUNT_KEY_PREFIX + commentId);
+            redisTemplate.opsForSet().add(RedisKey.DIRTY_COMMENT_LIKE_KEY, String.valueOf(commentId));
         }
     }
 
