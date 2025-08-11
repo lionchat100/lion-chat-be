78604f55cb11c7e16e3801707a9bb42153d1d508
W.llama <106076670+W-llama@users.noreply.github.com>
2025-08-11 14:42:47 +0900

    feat: 유저 좋아요 기능 + 알림기능

---
+++ src/main/java/com/lion/be/userlike/domain/entity/LikeCreatedEvent.java
@@ -0,0 +1,12 @@
+package com.lion.be.userlike.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LikeCreatedEvent {
+	private final Long fromUserId;
+	private final Long toUserId;
+	private final String fromUserName;
+}
