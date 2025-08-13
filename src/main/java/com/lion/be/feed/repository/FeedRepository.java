86b0d66111a7703eb46cc8af37e4eb6889be1e8c
LSH-kw0315 <tmdgjsdl1234@naver.com>
2025-08-13 12:03:48 +0900

    Fix: Feed, Comment User Role의 Banned도 필터링하도록 수정

--- src/main/java/com/lion/be/feed/repository/FeedRepository.java
+++ src/main/java/com/lion/be/feed/repository/FeedRepository.java
@@ -21,7 +21,7 @@ public interface FeedRepository extends JpaRepository<Feed, Long> {
                 u.name, u.id, u.imageUrl
             )
             FROM Feed f JOIN f.user u
-            WHERE f.isDeleted = false
+            WHERE f.isDeleted = false and u.role != 'BANNED'
             """)
     Slice<FeedResponse> fetchRecentFeedsFirst(Pageable pageable);
 
@@ -36,7 +36,7 @@ public interface FeedRepository extends JpaRepository<Feed, Long> {
                 u.name, u.id, u.imageUrl
             )
             FROM Feed f JOIN f.user u
-            WHERE f.isDeleted = false AND f.id < :lastId
+            WHERE f.isDeleted = false AND f.id < :lastId and u.role != 'BANNED'
             """)
     Slice<FeedResponse> fetchRecentFeedsAfter(Long lastId, Pageable pageable);
 
@@ -48,7 +48,7 @@ public interface FeedRepository extends JpaRepository<Feed, Long> {
                 u.name, u.id, u.imageUrl
             )
             FROM Feed f JOIN f.user u
-            WHERE f.isDeleted = false
+            WHERE f.isDeleted = false and u.role != 'BANNED'
             ORDER BY f.likeCount DESC, f.id DESC
             """)
     Slice<FeedResponse> fetchHotFeedsFirst(Pageable pageable);
@@ -61,7 +61,7 @@ public interface FeedRepository extends JpaRepository<Feed, Long> {
                 u.name, u.id, u.imageUrl
             )
             FROM Feed f JOIN f.user u
-            WHERE f.isDeleted = false
+            WHERE f.isDeleted = false and u.role != 'BANNED'
             AND (f.likeCount < :lastLikeCount OR (f.likeCount = :lastLikeCount AND f.id < :lastId))
             ORDER BY f.likeCount DESC, f.id DESC
             """)
