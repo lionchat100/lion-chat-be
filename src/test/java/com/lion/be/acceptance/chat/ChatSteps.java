c66620a3d188fa439092585150a27a18631cd9c4
devbattery <devbattery@outlook.com>
2025-08-13 11:56:14 +0900

    Fix #118: 병합 오류 해결

--- src/test/java/com/lion/be/acceptance/chat/ChatSteps.java
+++ src/test/java/com/lion/be/acceptance/chat/ChatSteps.java
@@ -70,6 +70,20 @@ public class ChatSteps {
                 .extract();
     }
 
+    public static ExtractableResponse<Response> 채팅방의_초기_메시지_목록을_조회한다(
+            String accessToken, Long chatRoomId, Long lastId, RequestSpecification spec) {
+        return RestAssured
+                .given().log().all()
+                .spec(spec)
+                .auth().oauth2(accessToken)
+                .queryParam("roomId", chatRoomId)
+                .queryParam("lastId", lastId)
+                .when()
+                .get("/api/chatrooms/chats/messages")
+                .then().log().all()
+                .extract();
+    }
+
     public static ExtractableResponse<Response> 채팅방의_메시지_목록을_조회한다(
             String accessToken, Long chatRoomId, Long lastId, RequestSpecification spec) {
         return RestAssured
