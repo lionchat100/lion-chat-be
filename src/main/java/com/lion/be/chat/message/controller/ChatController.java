321f58f634e0bf56a6f9403994ca020cc7d98ea3
LSH-kw0315 <tmdgjsdl1234@naver.com>
2025-08-16 22:25:04 +0900

    Fix: PreAuthorize 관련 문제 처리

--- src/main/java/com/lion/be/chat/message/controller/ChatController.java
+++ src/main/java/com/lion/be/chat/message/controller/ChatController.java
@@ -3,6 +3,10 @@ package com.lion.be.chat.message.controller;
 import com.lion.be.auth.domain.UserPrincipal;
 import com.lion.be.chat.message.domain.dto.ChatMessageResponse;
 import com.lion.be.chat.message.service.MessageService;
+import com.lion.be.chat.room.service.ChatRoomService;
+import com.lion.be.global.aop.ElapsedTime;
+import com.lion.be.global.exception.CustomException;
+import com.lion.be.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +26,7 @@ import java.util.List;
 public class ChatController {
 
     private final MessageService messageService;
+    private final ChatRoomService chatRoomService;
 
     /**
      * 채팅방의 이전 메시지 내역을 조회합니다.
@@ -30,13 +35,19 @@ public class ChatController {
      * @param lastId 마지막으로 조회한 메시지 ID
      * @return 이전 메시지 목록
      */
-    @PreAuthorize("@chatRoomUserRepository.existsById_ChatRoomIdAndId_UserId(#roomId, #userPrincipal.id)")
-    @GetMapping(value = "/messages", params = {"roomId", "lastId"})
+    //@PreAuthorize("@chatRoomUserRepository.existsById_ChatRoomIdAndId_UserId(#roomId, #userPrincipal.id)")
+    @GetMapping(value = "/messages")
     public ResponseEntity<List<ChatMessageResponse>> getMessageHistory(
             @AuthenticationPrincipal UserPrincipal userPrincipal,
             @RequestParam Long roomId,
-            @RequestParam(defaultValue = "0") Long lastId
+            @RequestParam(required = false) String lastId
     ) {
+        //PreAuthorize 대체 로직
+        if(!chatRoomService.checkUserExistsInChatRoom(roomId, userPrincipal.getId())) {
+            log.info("사용자가 채팅방에 존재하지 않습니다. roomId: {}, userId: {}", roomId, userPrincipal.getId());
+            throw new CustomException(ErrorCode.USER_UNAUTHORIZED);
+        }
+
         List<ChatMessageResponse> messages = messageService.findMessagesByIdAndLastId(roomId, lastId);
         return ResponseEntity.ok(messages);
     }
